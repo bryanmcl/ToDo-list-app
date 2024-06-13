0f9415defd7e7f0923636c7f9a276e08c9db38de
bryanmcl <bryanmcl0209@gmail.com>
2024-06-13 15:48:08 +0700

    [Add] Placeholder Component For Login, HomePage,, and Dash Board.  Create slicing for dashboard pages

---
+++ resources/js/components/Task.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="task-container">
+        <div class="task-title">
+            <input type="checkbox" />
+            <h2>Task 1 Water the plant or something</h2>
+        </div>
+        
+        <div class="label-wrapper">
+            <label class="date-label">
+                <ClockIcon class="icon" />
+                14/06/24 00:00
+            </label>
+            <label class="priority-label">Important</label>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ClockIcon } from '@heroicons/vue/16/solid';
+</script>
+
+<style scoped>
+.task-container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    margin: 15px 0;
+    padding: 15px;
+    height: 80px;
+
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px 2px var(--grey);
+}
+
+.task-container .task-title {
+    display: flex;
+    align-items: center;
+    width: 90%;
+    gap: 10px;
+    cursor: pointer;
+    flex-grow: 1;
+}
+
+.task-container h2 {
+    font-weight: 600;
+    font-size: 18px;
+}
+
+.label-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+
+}
+
+.label-wrapper .date-label {
+    color: var(--primary);
+}
+
+.icon {
+    vertical-align: bottom;
+    width: 20px;
+    height: 20px;
+}
+
+.priority-label {
+    padding: 5px 20px;
+    border-radius: 100px;
+    color: var(--light);
+    background-color: rgb(138, 21, 21);
+}
+
+.elipsis-button {
+    position: absolute;
+    top: 5px;
+    right: 12px;
+    border: none;
+    background: none;
+    padding: 5px;
+    font-weight: bold;
+    border-radius: 100%;
+    cursor: pointer;
+}
+
+.elipsis-button:hover {
+    background-color: var(--grey);
+}
+</style>
---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="dashboard-wrapper">
+        <Sidebar />
+        <div class="tasks-wrapper">
+            <h1>Task List</h1>
+            <button
+                @click="isModalVisible = true"
+            >
+                + Create New Task
+            </button>
+            <div class="task-list-container">
+                <Task />
+                <Task />
+                <Task />
+            </div>
+        </div>
+    </div>
+
+    <CreateModal 
+        :isVisible="isModalVisible"
+        @close="isModalVisible = false"
+    />
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import CreateModal from '../components/CreateModal.vue';
+import Sidebar from '../components/Sidebar.vue'
+import Task from '../components/Task.vue';
+
+const isModalVisible = ref(false);
+</script>
+
+<style scoped>
+.dashboard-wrapper {
+    display: flex;
+    min-height: 100vh;
+}
+
+.tasks-wrapper {
+    flex-grow: 1;
+    padding: 2rem;
+}
+
+button {
+    display: block;
+    padding: 10px;
+    margin-left: auto;
+    border: none;
+    border-radius: 5px;
+    background-color: var(--primary);
+    color: white;
+}
+
+.task-list-container {
+    margin-top: 1rem;
+}
+
+h1 {
+    color: var(--dark)
+}
+</style>
